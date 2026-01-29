d0ab32c46703e809964c57a7f138af5515f56229
glennhenry <glennhenrybdg05@gmail.com>
2026-01-29 15:11:43 +0700

    Update DefaultHandler to handle generic SocketMessage to avoid cast error on unregistered handler

--- src/main/kotlin/server/handler/impl/DefaultHandler.kt
+++ src/main/kotlin/server/handler/impl/DefaultHandler.kt
@@ -2,22 +2,19 @@ package server.handler.impl
 
 import server.handler.HandlerContext
 import server.handler.SocketMessageHandler
-import server.messaging.format.DefaultFormat
-import server.messaging.socket.DefaultMessage
+import server.messaging.socket.SocketMessage
 import utils.logging.Logger
 import kotlin.reflect.KClass
 
 /**
  * Default handler as the fallback for any unregistered socket handlers.
- *
- * This handler works together with [DefaultFormat] and [DefaultMessage].
  */
-class DefaultHandler : SocketMessageHandler<DefaultMessage> {
+class DefaultHandler : SocketMessageHandler<SocketMessage> {
     override val name: String = "DefaultHandler"
     override val messageType: String = "Default"
-    override val expectedMessageClass: KClass<DefaultMessage> = DefaultMessage::class
+    override val expectedMessageClass: KClass<SocketMessage> = SocketMessage::class
 
-    override suspend fun handle(ctx: HandlerContext<DefaultMessage>) = with(ctx) {
+    override suspend fun handle(ctx: HandlerContext<SocketMessage>) = with(ctx) {
         Logger.warn { "No handler registered/implemented for type=${message.type()}" }
     }
 }
